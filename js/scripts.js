3d316dfcd25fa90ce43879609e4e9c8effb70a1e
mkelley2 <[m_kelley2@yahoo.com]>
2017-05-15 12:26:22 -0700

    add resume link, more styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
-// $(document).ready(function() {
-//   $(window).scroll(function () {
-//       //if you hard code, then use console
-//       //.log to determine when you want the 
-//       //nav bar to stick.  
-//       console.log($(window).scrollTop())
-//     if ($(window).scrollTop() > 73) {
-//       $('#navbar').addClass('navbar-fixed');
-//     }
-//     if ($(window).scrollTop() < 74) {
-//       $('#navbar').removeClass('navbar-fixed');
-//     }
-//   });
-// });
+$(document).ready(function() {
+  $('.resume_link').click(function(){
+    $('.resume_holder').toggle();
+    $('.main_holder').toggle();
+    scroll(0,0);
+  })
+  
+  $('.other_link').click(function(){
+    $('.resume_holder').hide();
+    $('.main_holder').show();
+  })
+});
