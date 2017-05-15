51aa80e001f184b9ef8b0638f47ed5c1f55f1285
mkelley2 <[m_kelley2@yahoo.com]>
2017-05-15 11:21:24 -0700

    styling changes, moved about me and ocntact to main page

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// $(document).ready(function() {
+//   $(window).scroll(function () {
+//       //if you hard code, then use console
+//       //.log to determine when you want the 
+//       //nav bar to stick.  
+//       console.log($(window).scrollTop())
+//     if ($(window).scrollTop() > 73) {
+//       $('#navbar').addClass('navbar-fixed');
+//     }
+//     if ($(window).scrollTop() < 74) {
+//       $('#navbar').removeClass('navbar-fixed');
+//     }
+//   });
+// });
